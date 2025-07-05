40d68d3156342171dd43411abfcd477ad93bb2d0
charishma-v2509 <charishmavarkuti09@gmail.com>
2025-07-05 18:55:35 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+// server.js
+
+const app = require("./app");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
